65818c0399e954d673ee9f5a4d8ae14337e5870a
Mark Utting <m.utting@uq.edu.au>
2020-03-24 09:31:51 +1000

    Experiment with using records and extensions to model nodes.  Typing seems to be too restrictive.

---
+++ GraalRecords.thy
@@ -0,0 +1,24 @@
+theory GraalRecords
+imports Main
+begin
+
+record "IRNode" =
+  dummy :: int
+
+record IR2 = IRNode +
+  successors :: "IRNode list"
+  inputs :: "IRNode list"
+
+record ConstNode = IR2 +
+  val :: "int"
+
+definition node0 :: "IRNode" where
+  "node0 = \<lparr> dummy = 0 \<rparr>"
+
+definition node1 :: "IR2" where
+  "node1 = \<lparr> dummy = 0, successors = [], inputs = [] \<rparr>"
+
+definition one_node :: "ConstNode" where
+  "one_node = \<lparr> dummy = 0, successors = [node1], inputs = [], val = 1 \<rparr>"
+
+end

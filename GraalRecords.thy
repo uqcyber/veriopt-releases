c5bfedc8e1337338ff4eb64a4cb6a4b3df3d37fd
Mark Utting <m.utting@uq.edu.au>
2020-03-31 15:47:00 +1000

    More notes and explanations of options.

--- GraalRecords.thy
+++ GraalRecords.thy
@@ -1,5 +1,5 @@
 theory GraalRecords
-imports Main
+  imports Main
 begin
 
 record "IRNode" =
@@ -19,6 +19,12 @@ definition node1 :: "IR2" where
   "node1 = \<lparr> dummy = 0, successors = [], inputs = [] \<rparr>"
 
 definition one_node :: "ConstNode" where
-  "one_node = \<lparr> dummy = 0, successors = [node1], inputs = [], val = 1 \<rparr>"
+  "one_node = \<lparr> dummy = 0, successors = [node0], inputs = [], val = 1 \<rparr>"
+
+(* Unfortunately, the record type system does not seem to allow
+   different subtypes of records to appear where the supertype is expected.
+*)
+definition polymorphic_node :: "ConstNode" where
+  "polymorphic_node = \<lparr> dummy = 0, successors = [node1], inputs = [], val = 1 \<rparr>"
 
 end

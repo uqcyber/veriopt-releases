37f6a969f6e07518a3434725f64b147b060c722a
Mark Utting <m.utting@uq.edu.au>
2020-07-03 16:20:21 +1000

    Change the optimisation to separate applicability checker, and a deterministic do-it-here function.  Fixed an error in the IF successors.

--- ExpandLogicPhase.thy
+++ ExpandLogicPhase.thy
@@ -2,36 +2,52 @@ theory ExpandLogicPhase
 imports AbsGraph
 begin
 
-(* One transformation from the ExpandLogicPhase optimisation. *)
+(* One transformation from the ExpandLogicPhase optimisation.
+   The transformation is modelled as an applicability test:
 
-fun opt_expand_logic :: "IRGraph \<Rightarrow> IRGraph \<Rightarrow> bool" where
-  "opt_expand_logic gin gout =
-    (\<exists> shortCircuitOr \<in> g_ids gin. is_ShortCircuitOrNode(g_nodes gin shortCircuitOr) \<and>
-    (\<exists> x \<in> g_ids gin.
-    (\<exists> y \<in> g_ids gin. g_inputs gin shortCircuitOr = [x, y] \<and>
-    (\<exists> ifNode \<in> g_ids gin. g_nodes gin ifNode = IfNode \<and>
-       g_inputs gin ifNode = [shortCircuitOr] \<and>
-    (\<exists> trueTarget \<in> g_ids gin.
-    (\<exists> falseTarget \<in> g_ids gin. g_successors gin ifNode = [trueTarget, falseTarget] \<and>
+      'can_expand_logic graph'
 
-    (\<exists> firstIf firstTrueTarget firstTrueEnd secondIfBegin
-       secondIf secondTrueTarget secondTrueEnd trueTargetMerge . 
-      [firstIf, firstTrueTarget, firstTrueEnd, secondIfBegin,
-       secondIf, secondTrueTarget, secondTrueEnd, trueTargetMerge] = next_ids gin 8 \<and>
+   that returns the set of IfNodes where this optimisation can be applied,
+   and a function that does the transformation (at a given node 'ifnode')
+
+      'do_expand_logic ifnode graph'
+
+   and returns the transformed graph.
+*)
+fun can_expand_logic :: "IRGraph \<Rightarrow> ID set" where
+  "can_expand_logic gin =
+    {ifNode. ifNode \<in> g_ids gin \<and> g_nodes gin ifNode = IfNode \<and>
+     (let shortCircuitOr = hd (g_inputs gin ifNode) in
+      is_ShortCircuitOrNode(g_nodes gin shortCircuitOr))
+    }"
+
+fun do_expand_logic :: "ID \<Rightarrow> IRGraph \<Rightarrow> IRGraph" where
+  "do_expand_logic ifNode gin =
+    (THE gout.
+      (let shortCircuitOr = hd (g_inputs gin ifNode) in
+      (\<exists> x \<in> g_ids gin.
+      (\<exists> y \<in> g_ids gin.
+        g_inputs gin shortCircuitOr = [x, y] \<and>
+      (\<exists> trueTarget \<in> g_ids gin.
+      (\<exists> falseTarget \<in> g_ids gin.
+        g_successors gin ifNode = [trueTarget, falseTarget] \<and>
+      (\<exists> firstIf firstTrueTarget firstTrueEnd secondIfBegin
+         secondIf secondTrueTarget secondTrueEnd trueTargetMerge . 
+        [firstIf, firstTrueTarget, firstTrueEnd, secondIfBegin,
+         secondIf, secondTrueTarget, secondTrueEnd, trueTargetMerge] = next_ids gin 8 \<and>
     gout =
-      (add_node firstIf          IfNode    [x]  [secondIfBegin]
+      (add_node firstIf          IfNode    [x]  [firstTrueTarget, secondIfBegin]
       (add_node firstTrueTarget  BeginNode []   [firstTrueEnd]
       (add_node firstTrueEnd     EndNode   []   []
       (add_node secondIfBegin    BeginNode []   [secondIf]
-      (add_node secondIf         IfNode    [y]  [secondTrueTarget]
+      (add_node secondIf         IfNode    [y]  [secondTrueTarget, falseTarget]
       (add_node secondTrueTarget BeginNode []   [secondTrueEnd]
       (add_node secondTrueEnd    EndNode   []   []
       (add_node trueTargetMerge  MergeNode [firstTrueEnd,secondTrueEnd] [trueTarget]
       (del_node ifNode
       (del_node shortCircuitOr
-       gin)))
-    ))))))))))))))"
-
+       gin))))))))))
+    )))))))"
 
 (* ShortCutOr Example 1: (based on Figure 2 from Graal_IR_2013_10)
   ================================================================
@@ -86,41 +102,49 @@ lemma "wff_graph eg_short_cut_or1"
   (* nitpick [verbose, card=1-20] *)
   by auto
 
-(*
-schematic_goal opt1: "opt_expand_logic eg_short_cut_or1 ?OUT"
+
+(* See where this optimisation can be applied?  Returns {5}. *)
+value "can_expand_logic eg_short_cut_or1"
+
+schematic_goal opt1: "do_expand_logic 5 eg_short_cut_or1 = ?OUT"
   unfolding eg_short_cut_or1_def
-  apply (simp add: insert_Diff_if)   (* takes about 15 minutes! *)
+  apply (simp add: insert_Diff_if)   (* took about 15 minutes with relational defn! *)
   done
 
 thm opt1
-*)
+
 
 (* This calculated ?OUT as follows: *)
-definition out1 :: IRGraph where
-  "out1 =  (Graph {15, 16, 17, 18, 19, 20, 21, 22, 0, 14, 13, 12, 11, 10, 9, 8, 7, 6, 2, Suc 0, 0}
-   ((\<lambda>i. StartNode)
-    (0 := StartNode, Suc 0 := ParameterNode 0, 2 := ParameterNode 1, 6 := BeginNode, 7 := EndNode,
-     8 := BeginNode, 9 := EndNode, 10 := MergeNode, 11 := ConstantNode 42, 12 := ConstantNode 0,
-     13 := PhiNode, 14 := ReturnNode, 3 := StartNode, 5 := StartNode, 22 := MergeNode,
-     21 := EndNode, 20 := BeginNode, 19 := IfNode, 18 := BeginNode, 17 := EndNode, 16 := BeginNode,
-     15 := IfNode))
-   ((\<lambda>i. [])
-    (0 := [], Suc 0 := [], 2 := [], 6 := [], 7 := [], 8 := [], 9 := [], 10 := [7, 9], 11 := [],
-     12 := [], 13 := [10, 11, 12], 14 := [13], 3 := [], 5 := [], 22 := [17, 21], 21 := [],
-     20 := [], 19 := [2], 18 := [], 17 := [], 16 := [], 15 := [Suc 0]))
-   ((\<lambda>i. [])
-    (0 := [5], Suc 0 := [], 2 := [], 6 := [7], 7 := [], 8 := [9], 9 := [], 10 := [14], 11 := [],
-     12 := [], 13 := [], 14 := [], 3 := [], 5 := [], 22 := [6], 21 := [], 20 := [21], 19 := [20],
-     18 := [19], 17 := [], 16 := [17], 15 := [18])))"
+definition eg_out1 :: IRGraph where
+  "eg_out1 = Graph 
+ {15, 16, 17, 18, 19, 20, 21, 22, 0, 14, 13, 12, 11, 10, 9, 8, 7, 6, 2, Suc 0, 0}
+ ((\<lambda>i. StartNode)
+  (0 := StartNode, Suc 0 := ParameterNode 0, 2 := ParameterNode 1, 6 := BeginNode, 7 := EndNode,
+   8 := BeginNode, 9 := EndNode, 10 := MergeNode, 11 := ConstantNode 42, 12 := ConstantNode 0,
+   13 := PhiNode, 14 := ReturnNode, 3 := StartNode, 5 := StartNode, 22 := MergeNode, 21 := EndNode,
+   20 := BeginNode, 19 := IfNode, 18 := BeginNode, 17 := EndNode, 16 := BeginNode, 15 := IfNode))
+ ((\<lambda>i. [])
+  (0 := [], Suc 0 := [], 2 := [], 6 := [], 7 := [], 8 := [], 9 := [], 10 := [7, 9], 11 := [],
+   12 := [], 13 := [10, 11, 12], 14 := [13], 3 := [], 5 := [], 22 := [17, 21], 21 := [], 20 := [],
+   19 := [2], 18 := [], 17 := [], 16 := [], 15 := [Suc 0]))
+ ((\<lambda>i. [])
+  (0 := [5], Suc 0 := [], 2 := [], 6 := [7], 7 := [], 8 := [9], 9 := [], 10 := [14], 11 := [],
+   12 := [], 13 := [], 14 := [], 3 := [], 5 := [], 22 := [6], 21 := [], 20 := [21], 19 := [20, 8],
+   18 := [19], 17 := [], 16 := [17], 15 := [16, 18]))
+"
 
-lemma check1: "opt_expand_logic eg_short_cut_or1 out1"
-  unfolding eg_short_cut_or1_def out1_def
-  (* apply code_simp --- gives wellsortedness error *)
+lemma check1: "do_expand_logic 5 eg_short_cut_or1 = eg_out1"
+  unfolding eg_short_cut_or1_def eg_out1_def
+  (* apply code_simp  --- gives wellsortedness error *)
   apply (simp add: Let_def)   
   
-  (* takes about 4:30 mins *)
+  (* Took about 4:30 mins with old relational-version of the optimisation.
+     Takes about 25 secs to simplify this functional version
+     (or 1:20 if you add insert_Diff_if into the above simp.)  *)
 
-  apply (simp add: insert_Diff_if)
+  apply (simp  add: insert_Diff_if [simp_trace])
   done
+
+print_bundles
 
 end

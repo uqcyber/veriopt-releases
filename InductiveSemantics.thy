aff7df1b62370e0b754709a9bc865e7a3ce6ecc3
Brae Webb <email@braewebb.com>
2020-09-09 19:03:10 +1000

    Refactoring Isabelle Semantics

--- InductiveSemantics.thy
+++ InductiveSemantics.thy
@@ -24,38 +24,27 @@ type_synonym EvalNode = "ID \<times> IRNode \<times> EvalState"
 
 (* Get the type of node for a node id in an eval state *)
 fun get_node :: "ID \<Rightarrow> EvalState \<Rightarrow> IRNode" where
-  "get_node nid state = ((g_nodes (s_graph state)) nid)"
+  "get_node nid s = ((g_nodes (s_graph s)) nid)"
+
+fun to_eval_node :: "ID \<Rightarrow> EvalState \<Rightarrow> EvalNode" where
+  "to_eval_node nid s = (nid, (get_node nid s), s)"
 
 
-fun get_input :: "ID \<Rightarrow> EvalState \<Rightarrow> nat \<Rightarrow> ID" where 
-  "get_input nid state i = ((g_inputs (s_graph state) nid)!i)"
+fun get_input :: "ID \<Rightarrow> nat \<Rightarrow> EvalState \<Rightarrow> ID" where 
+  "get_input nid i s = ((g_inputs (s_graph s) nid)!i)"
+fun input :: "ID \<Rightarrow> nat \<Rightarrow> EvalState \<Rightarrow> EvalNode" where
+  "input nid i s = (to_eval_node (get_input nid i s) s)"
 
-(* Get the nth input edge of a node id in an eval state *)
-fun input :: "ID \<Rightarrow> EvalState \<Rightarrow> nat \<Rightarrow> EvalNode" where
-  "input nid state i = 
-    (let next_id = (get_input nid state i) in
-    (next_id, (get_node next_id state), state))"
+fun get_successor :: "ID \<Rightarrow> nat \<Rightarrow> EvalState \<Rightarrow> ID" where
+  "get_successor nid i s = ((g_successors (s_graph s) nid)!i)"
+fun successor :: "ID \<Rightarrow> nat \<Rightarrow> EvalState \<Rightarrow> EvalNode" where
+  "successor nid i s = (to_eval_node (get_successor nid i s) s)"
 
-
-fun get_successor :: "ID \<Rightarrow> EvalState \<Rightarrow> nat \<Rightarrow> ID" where
-  "get_successor nid state i = ((g_successors (s_graph state) nid)!i)"
-
-(* Get the nth successor edge of a node id in an eval state *)
-fun successor :: "ID \<Rightarrow> EvalState \<Rightarrow> nat \<Rightarrow> EvalNode" where
-  "successor nid state i =
-    (let next_id = (get_successor nid state i) in
-    (next_id, (get_node next_id state), state))"
-
-
-fun get_usage :: "ID \<Rightarrow> EvalState \<Rightarrow> nat \<Rightarrow> ID" where
-  "get_usage nid state i =
-    ((sorted_list_of_set (g_usages (s_graph state) nid))!i)"
-
-(* Get the nth usage edge of a node id in an eval state *)
-fun usage :: "ID \<Rightarrow> EvalState \<Rightarrow> nat \<Rightarrow> EvalNode"  where
-  "usage nid state i =
-    (let use = (get_usage nid state i) in 
-    (use, (get_node use state), state))"
+fun get_usage :: "ID \<Rightarrow> nat \<Rightarrow> EvalState \<Rightarrow> ID" where
+  "get_usage nid i s =
+    ((sorted_list_of_set (g_usages (s_graph s) nid))!i)"
+fun usage :: "ID \<Rightarrow> nat \<Rightarrow> EvalState \<Rightarrow> EvalNode"  where
+  "usage nid i s = (to_eval_node (get_usage nid i s) s)"
 
 (*
    ====
@@ -106,14 +95,6 @@ definition merge_nodes :: "IRNode set" where
 definition end_nodes :: "IRNode set" where
   "end_nodes = {EndNode, LoopEndNode}"
 
-(* 
-update_state f a b = f'
-in the function f', passing a results in b, passing
-any other parameter, x, results in f(x)
-*)
-fun update_state :: "'a \<Rightarrow> 'b \<Rightarrow> ('a \<Rightarrow> 'b) \<Rightarrow> ('a \<Rightarrow> 'b)" where
-  "update_state ident val state = (\<lambda> x. (if x = ident then val else (state x)))"
-
 (* Yoinked from https://www.isa-afp.org/browser_info/Isabelle2012/HOL/List-Index/List_Index.html*)
 fun find_index :: "'a \<Rightarrow> 'a list \<Rightarrow> nat" where
   "find_index _ [] = 0" |
@@ -123,17 +104,17 @@ fun find_index :: "'a \<Rightarrow> 'a list \<Rightarrow> nat" where
 fun phi_list :: "EvalNode \<Rightarrow> EvalNode list" where
   "phi_list (n, node, s) = 
       (map (\<lambda>x. (x, (get_node x s), s))
-        (filter (\<lambda>x.((get_node x s) = PhiNode))
+        (filter (\<lambda>x.((get_node x s)=PhiNode))
           (sorted_list_of_set (g_usages (s_graph s) n))))"
 
 fun input_index :: "EvalState \<Rightarrow> ID \<Rightarrow> ID \<Rightarrow> nat" where
   "input_index s n n' = find_index n' (g_inputs (s_graph s) n)"
 
 fun phi_inputs :: "nat \<Rightarrow> EvalState \<Rightarrow> EvalNode list \<Rightarrow> EvalNode list" where
-  "phi_inputs i s nodes = (map (\<lambda>(n, _, s). (input n s (i + 1))) nodes)"
+  "phi_inputs i s nodes = (map (\<lambda>(n, _, s). (input n (i + 1) s)) nodes)"
 
 fun update_phi :: "ID \<Rightarrow> Value \<Rightarrow> EvalState \<Rightarrow> EvalState" where
-  "update_phi nid val s = (update_s_phi (\<lambda>_. (update_state nid val (s_phi s))) s)"
+  "update_phi nid val s = (update_s_phi (\<lambda>_. ((s_phi s)(nid := val))) s)"
 
 fun set_phis :: "EvalState \<Rightarrow> EvalNode list \<Rightarrow> Value list \<Rightarrow> EvalState" where
   "set_phis s [] [] = s" |
@@ -152,16 +133,16 @@ inductive
 
 
   StartNode:
-  "\<lbrakk>(successor nid s 0) \<mapsto> succ\<rbrakk> 
+  "\<lbrakk>(successor nid 0 s) \<mapsto> succ\<rbrakk> 
     \<Longrightarrow> (nid, StartNode, s) \<mapsto> succ" |
 
   BeginNode: 
-  "\<lbrakk>(successor nid s 0) \<mapsto> succ\<rbrakk>
+  "\<lbrakk>(successor nid 0 s) \<mapsto> succ\<rbrakk>
     \<Longrightarrow> (nid, BeginNode, s) \<mapsto> succ" |
 
   MergeNodes:
   "\<lbrakk>node \<in> merge_nodes;
-    (successor nid s 0) \<mapsto> succ\<rbrakk> 
+    (successor nid 0 s) \<mapsto> succ\<rbrakk> 
     \<Longrightarrow> (nid, node, s) \<mapsto> succ" |
 
   ParameterNode:
@@ -172,29 +153,29 @@ inductive
 
   UnaryNode:
   "\<lbrakk>node \<in> unary_nodes;
-    (input nid s 0) \<mapsto> (s', v)\<rbrakk> 
+    (input nid 0 s) \<mapsto> (s', v)\<rbrakk> 
     \<Longrightarrow> (nid, node, s) \<mapsto> (s', (unary_expr node v))" |
 
   BinaryNode:
   "\<lbrakk>node \<in> binary_nodes;
-    (input nid s 0) \<mapsto> (s', v1);
-    (input nid s' 1) \<mapsto> (s'', v2)\<rbrakk> 
+    (input nid 0 s) \<mapsto> (s', v1);
+    (input nid 1 s') \<mapsto> (s'', v2)\<rbrakk> 
     \<Longrightarrow> (nid, node, s) \<mapsto> (s'', (binary_expr node v1 v2))" |
 
   IfNodeTrue:
-  "\<lbrakk>(input nid s 0) \<mapsto> (s', cond);
+  "\<lbrakk>(input nid 0 s) \<mapsto> (s', cond);
     (val_to_bool cond);
-    (successor nid s' 0) \<mapsto> s''\<rbrakk> 
+    (successor nid 0 s') \<mapsto> s''\<rbrakk> 
     \<Longrightarrow> (nid, IfNode, s) \<mapsto> s''" |
 
   IfNodeFalse:
-  "\<lbrakk>(input nid s 0) \<mapsto> (s', cond);
+  "\<lbrakk>(input nid 0 s) \<mapsto> (s', cond);
     (\<not>(val_to_bool cond));
-    (successor nid s' 1) \<mapsto> s''\<rbrakk> 
+    (successor nid 1 s') \<mapsto> s''\<rbrakk> 
     \<Longrightarrow> (nid, IfNode, s) \<mapsto> s''" |
 
   ReturnNode:
-  "\<lbrakk>(input nid s 0) \<mapsto> (s', v)\<rbrakk> 
+  "\<lbrakk>(input nid 0 s) \<mapsto> (s', v)\<rbrakk> 
     \<Longrightarrow> (nid, ReturnNode, s) \<mapsto> (s', v)" |
  
 
@@ -203,7 +184,7 @@ inductive
   EndNodes:
   "\<lbrakk>node \<in> end_nodes;
 
-    (merge, merge_node, _) = (usage nid s 0);
+    (merge, merge_node, _) = (usage nid 0 s);
     i = (input_index s merge nid);
 
     phis = (phi_list (merge, merge_node, s));
@@ -227,30 +208,18 @@ code_pred eval_all .
 fun new_state :: "IRGraph \<Rightarrow> Value list \<Rightarrow> EvalState" where
   "new_state graph params = (EvalState graph params (\<lambda> x. UndefVal))"
 
-definition ex_graph :: IRGraph where
-  "ex_graph =
+definition double_param_graph :: IRGraph where
+  "double_param_graph =
     (add_node 3 ReturnNode [2] []
     (add_node 2 AddNode [1, 1] []
     (add_node 1 (ParameterNode 0) [] []
     (add_node 0 StartNode [] [3]
     empty_graph))))"
-lemma "wff_graph ex_graph"
-  unfolding ex_graph_def by simp
+lemma "wff_graph double_param_graph"
+  unfolding double_param_graph_def by simp
 
-definition ex_graph2 :: IRGraph where
-  "ex_graph2 =
-    (add_node 5 ReturnNode [1] []
-    (add_node 4 ReturnNode [2] []
-    (add_node 3 IfNode [1] [4, 5]
-    (add_node 2 (ConstantNode 30) [] []
-    (add_node 1 (ConstantNode 1) [] []
-    (add_node 0 StartNode [] [3]
-    empty_graph))))))"
-lemma "wff_graph ex_graph2"
-  unfolding ex_graph2_def by simp
-
-definition eg3 :: IRGraph where
-  "eg3 =
+definition simple_if_graph :: IRGraph where
+  "simple_if_graph =
     (add_node 12 ReturnNode [11] []
     (add_node 11 PhiNode [10,9,7] []
     (add_node 10 MergeNode [5,6] [12]
@@ -265,41 +234,23 @@ definition eg3 :: IRGraph where
     (add_node 1 (ParameterNode 0) [] []
     (add_node 0 StartNode [] [2]
     empty_graph)))))))))))))"
-lemma "wff_graph eg3"
-  unfolding eg3_def by simp
+lemma "wff_graph simple_if_graph"
+  unfolding simple_if_graph_def by simp
 
 notepad begin 
-  have "input_index (new_state eg3 []) 10 5 = 0" by eval
-  have "input_index (new_state eg3 []) 10 6 = 1" by eval
-  have "input_index (new_state eg3 []) 11 10 = 0" by eval
-  have "input_index (new_state eg3 []) 11 9 = 1" by eval
-  have "input_index (new_state eg3 []) 11 7 = 2" by eval
-  have "input_index (new_state eg3 []) 11 20 = 3" by eval
+  have "input_index (new_state simple_if_graph []) 10 5 = 0" by eval
+  have "input_index (new_state simple_if_graph []) 10 6 = 1" by eval
+  have "input_index (new_state simple_if_graph []) 11 10 = 0" by eval
+  have "input_index (new_state simple_if_graph []) 11 9 = 1" by eval
+  have "input_index (new_state simple_if_graph []) 11 7 = 2" by eval
+  have "input_index (new_state simple_if_graph []) 11 20 = 3" by eval
 end
 
-value "usage 5 (new_state eg3 []) 0" (* (10, MergeNode, ...) *)
-value "usage 6 (new_state eg3 []) 0" (* (10, MergeNode, ...) *)
+value "usage 5 0 (new_state simple_if_graph [])" (* (10, MergeNode, ...) *)
+value "usage 6 0 (new_state simple_if_graph [])" (* (10, MergeNode, ...) *)
 
-value "phi_list (10, MergeNode, (new_state eg3 []))" (* [(11, PhiNode, ...)] *)
+value "phi_list (10, MergeNode, (new_state simple_if_graph []))" (* [(11, PhiNode, ...)] *)
 
-definition eg4 :: IRGraph where
-  "eg4 =
-    (add_node 14 (ParameterNode 0) [] []
-    (add_node 13 ReturnNode [2] []
-    (add_node 12 EndNode [] [13]
-    (add_node 11 BeginNode [] [12]
-    (add_node 10 LoopEndNode [7] []
-    (add_node 9 BeginNode [] [10]
-    (add_node 8 IfNode [3] [9,11]
-    (add_node 7 LoopBeginNode [1] [8]
-    (add_node 6 (ConstantNode 4) [] []
-    (add_node 5 (ConstantNode 0) [] []
-    (add_node 4 AddNode [2,6] []
-    (add_node 3 IntegerLessThanNode [2,14] []
-    (add_node 2 PhiNode [7,5,4] [] 
-    (add_node 1 EndNode [] []
-    (add_node 0 StartNode [] [1]
-    empty_graph)))))))))))))))"
 
 (* Currently causes a wellsortedness error which is resolved by removing
  * all IRNode constructors which have parameters e.g (ParameterNode i)
@@ -315,14 +266,12 @@ definition eg4 :: IRGraph where
  * inductive rules
  *)
 
-values "{(s, v). (0, StartNode, (new_state ex_graph [IntVal 5])) \<mapsto> (s, v)}"
+(* IntVal 10 *)
+values "{(s, v). (0, StartNode, (new_state double_param_graph [IntVal 5])) \<mapsto> (s, v)}"
 
-values "{(s, v). (0, StartNode, (new_state ex_graph2 [])) \<mapsto> (s, v)}"
-
-values "{(s, v). (0, StartNode, (new_state eg3 [IntVal 0, IntVal 20, IntVal 100])) \<mapsto> (s, v)}"
-
-export_code eval in SML
-
-values "{(s, v). (0, StartNode, (new_state eg4 [IntVal 0, IntVal 20, IntVal 100])) \<mapsto> (s, v)}"
+(* IntVal 20 *)
+values "{(s, v). (0, StartNode, (new_state simple_if_graph [IntVal 0, IntVal 20, IntVal 100])) \<mapsto> (s, v)}"
+(* IntVal 120 *)
+values "{(s, v). (0, StartNode, (new_state simple_if_graph [IntVal 1, IntVal 20, IntVal 100])) \<mapsto> (s, v)}"
 
 end

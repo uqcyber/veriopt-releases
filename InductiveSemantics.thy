0c702388cfd892590dc65c111e852d82c07eb9fa
BraeWebb <email@braewebb.com>
2020-09-12 15:02:06 +1000

    Fixed Control Flow Examples

--- InductiveSemantics.thy
+++ InductiveSemantics.thy
@@ -64,8 +64,8 @@ fun phi_list :: "Graph \<Rightarrow> ID \<Rightarrow> ID list" where
     (filter (\<lambda>x.((kind g x)=PhiNode))
       (sorted_list_of_set (usagex g nid)))"
 
-fun input_index :: "IRGraph \<Rightarrow> ID \<Rightarrow> ID \<Rightarrow> nat" where
-  "input_index g n n' = find_index n' (g_inputs g n)"
+fun input_index :: "Graph \<Rightarrow> ID \<Rightarrow> ID \<Rightarrow> nat" where
+  "input_index g n n' = find_index n' (inp g n)"
 
 fun phi_inputs :: "Graph \<Rightarrow> nat \<Rightarrow> ID list \<Rightarrow> ID list" where
   "phi_inputs g i nodes = (map (\<lambda>n. (inp g n)!(i + 1)) nodes)"
@@ -76,8 +76,9 @@ fun set_phis :: "ID list \<Rightarrow> Value list \<Rightarrow> MapState \<Right
   "set_phis [] (v # vs) m = m" |
   "set_phis (x # xs) [] m = m"
 
-fun inpu :: "Graph \<Rightarrow> ID \<Rightarrow> nat \<Rightarrow> ID" where
-  "inpu g nid i = (inp g nid)!i"
+
+fun usage :: "Graph \<Rightarrow> ID \<Rightarrow> nat \<Rightarrow> ID" where
+  "usage g nid i = (sorted_list_of_set (usagex g nid))!i"
 
 
 inductive
@@ -92,6 +93,7 @@ inductive
 
   "g [] m \<longmapsto> []" |
   "\<lbrakk>g (nid, m) \<rightarrow> v; g xs m \<longmapsto> vs\<rbrakk> \<Longrightarrow> g (nid # xs) m \<longmapsto> (v # vs)" |
+
   "\<lbrakk>g \<turnstile> (nid, m) \<mapsto> (nid', m')\<rbrakk> \<Longrightarrow> g \<turnstile> (nid, m) # xs \<longrightarrow> (nid', m') # xs" |
 
   CallNodeStep:
@@ -137,24 +139,25 @@ inductive
   BinaryNode:
   "\<lbrakk>
     kind g nid \<in> binary_nodes;
-    g (inpu g nid 0, m) \<rightarrow> v1;
-    g (inpu g nid 1, m) \<rightarrow> v2\<rbrakk> 
+    g ((inp g nid)!0, m) \<rightarrow> v1;
+    g ((inp g nid)!1, m) \<rightarrow> v2\<rbrakk> 
     \<Longrightarrow> g (nid, m) \<rightarrow> (binary_expr (kind g nid) v1 v2)" |
 
   IfNode:
   "\<lbrakk>kind g nid = IfNode;
     g ((inp g nid)!0, m) \<rightarrow> (IntVal cond)\<rbrakk>
-   \<Longrightarrow> g \<turnstile> (nid, m) \<mapsto> ((succ g nid)!(if val_to_bool cond then 0 else 1), m)"  
+   \<Longrightarrow> g \<turnstile> (nid, m) \<mapsto> ((succ g nid)!(if val_to_bool cond then 0 else 1), m)" |  
 
   (* Solution to the eval_all but the evalution gives up :(
    * \<forall> i. i < length inputs \<longrightarrow> (\<exists> s' . (inputs!i \<mapsto> (s',vs!i))); *)
-(*
-  EndNodes:
-  "\<lbrakk>node = kind g nid;
-    node \<in> end_nodes;
-    kind g merge = MergeNode;
 
-    (inp g merge)!i = nid;
+  EndNodes:
+  "\<lbrakk>kind g nid \<in> end_nodes;
+
+    merge = usage g nid 0;
+    kind g merge \<in> merge_nodes;
+
+    i = input_index g merge nid;
 
     phis = (phi_list g merge);
     inputs = (phi_inputs g i phis);
@@ -166,13 +169,20 @@ inductive
   PhiNode:
   "\<lbrakk>kind g nid = PhiNode\<rbrakk>
    \<Longrightarrow>  g (nid, m) \<rightarrow> m nid"
-*)
+
 
 code_pred step .
 code_pred step_top .
 code_pred eval_exp .
 code_pred eval_all .
 
+fun ir_to_graph_i :: "IRGraph \<Rightarrow> ID list \<Rightarrow> Graph \<Rightarrow> Graph" where
+  "ir_to_graph_i ir [] g = g" |
+  "ir_to_graph_i ir (n # ns) g = fmupd n (N (g_nodes ir n) (g_inputs ir n) (g_successors ir n)) (ir_to_graph_i ir ns g)"
+
+fun ir_to_graph :: "IRGraph \<Rightarrow> Graph" where
+  "ir_to_graph g = ir_to_graph_i g 
+      (sorted_list_of_set (g_ids g)) fmempty"
 
 (* Example graph evaluation *)
 fun new_map_i :: "IRGraph \<Rightarrow> Value list \<Rightarrow> ID list \<Rightarrow> MapState \<Rightarrow> MapState" where
@@ -199,6 +209,9 @@ lemma "wff_graph simple_return_graph"
 definition simple_return_map :: "MapState" where
   "simple_return_map = new_map simple_return_graph []"
 
+definition simple_return :: "Graph" where
+  "simple_return = (ir_to_graph simple_return_graph)"
+
 definition double_param_graph :: IRGraph where
   "double_param_graph =
     (add_node 3 ReturnNode [2] []
@@ -211,6 +224,9 @@ lemma "wff_graph double_param_graph"
 
 definition double_param_map :: "MapState" where
   "double_param_map = new_map double_param_graph [IntVal 5]"
+
+definition double_param :: "Graph" where
+  "double_param = (ir_to_graph double_param_graph)"
 
 definition simple_if_graph :: IRGraph where
   "simple_if_graph =
@@ -234,22 +250,17 @@ lemma "wff_graph simple_if_graph"
 definition simple_if_map :: "MapState" where
   "simple_if_map = new_map simple_if_graph [IntVal 0, IntVal 20, IntVal 100]"
 
-fun ir_to_graph_i :: "IRGraph \<Rightarrow> ID list \<Rightarrow> Graph \<Rightarrow> Graph" where
-  "ir_to_graph_i ir [] g = g" |
-  "ir_to_graph_i ir (n # ns) g = (ir_to_graph_i ir ns g)(n := (N (g_nodes ir n) (g_inputs ir n) (g_successors ir n)))"
-
-fun ir_to_graph :: "IRGraph \<Rightarrow> Graph" where
-  "ir_to_graph g = ir_to_graph_i g 
-      (sorted_list_of_set (g_ids g)) (\<lambda>x . (N StartNode [] []))"
+definition simple_if :: "Graph" where
+  "simple_if = (ir_to_graph simple_if_graph)"
 
 notepad begin 
-  have "input_index simple_if_graph 10 5 = 0" by eval
-  have "input_index simple_if_graph 10 6 = 1" by eval
-  have "input_index simple_if_graph 11 10 = 0" by eval
-  have "input_index simple_if_graph 11 9 = 1" by eval
-  have "input_index simple_if_graph 11 7 = 2" by eval
-  have "input_index simple_if_graph 11 20 = 3" by eval
-  have "phi_list (ir_to_graph simple_if_graph) 10 = [11]" by eval
+  have "input_index simple_if 10 5 = 0" by eval
+  have "input_index simple_if 10 6 = 1" by eval
+  have "input_index simple_if 11 10 = 0" by eval
+  have "input_index simple_if 11 9 = 1" by eval
+  have "input_index simple_if 11 7 = 2" by eval
+  have "input_index simple_if 11 20 = 3" by eval
+  have "phi_list simple_if 10 = [11]" by eval
 end
 
 
@@ -263,30 +274,34 @@ inductive eval_graph :: "Graph \<Rightarrow> ID \<times> MapState \<Rightarrow> 
 
 code_pred "eval_graph" .
 
-(* IntVal 10 *)
-definition simple_return :: "Graph" where
-  "simple_return = (ir_to_graph simple_return_graph)"
+
+(* Simple Return *)
 values "{(n, map m [0]) |n m. simple_return \<turnstile> (0, simple_return_map) \<mapsto> (n, m)}"
 values "{(n, map m [0]) |n m. simple_return \<turnstile> (2, simple_return_map) \<mapsto> (n, m)}"
 value "(inp simple_return 2)!0"
 values "{v |v. simple_return (1, simple_return_map) \<rightarrow> v}"
 
+(* IntVal 42 *)
 values "{map m [0] |n m. eval_graph simple_return (0, double_param_map) (n, m)}"
 
-definition double_param :: "Graph" where
-  "double_param = (ir_to_graph double_param_graph)"
+(* Double Param *)
 values "{(n, map m [0]) |n m. double_param \<turnstile> (0, double_param_map) \<mapsto> (n, m)}"
 values "{(n, map m [0]) |n m. double_param \<turnstile> (3, double_param_map) \<mapsto> (n, m)}"
 
+(* IntVal 10 *)
 values "{map m [0] |n m. eval_graph double_param (0, double_param_map) (n, m)}"
 
-definition simple_if :: "Graph" where
-  "simple_if = (ir_to_graph simple_if_graph)"
+(* Simple If *)
 (* IntVal 20 *)
 values "{(n, map m [0]) |n m. simple_if \<turnstile> (0, simple_if_map) \<mapsto> (n, m)}"
-values "{map m [0] |n m. eval_graph simple_if_graph (0, StartNode) simple_if_map (n, m)}"
+values "{(n, map m [0]) |n m. simple_if \<turnstile> (2, simple_if_map) \<mapsto> (n, m)}"
+values "{(n, map m [0]) |n m. simple_if \<turnstile> (4, simple_if_map) \<mapsto> (n, m)}"
+values "{(n, map m [0]) |n m. simple_if \<turnstile> (6, simple_if_map) \<mapsto> (n, m)}"
+
+
+values "{map m [0] |n m. eval_graph simple_if (0, simple_if_map) (n, m)}"
 (* IntVal 120 *)
-values "{(n, map m [0]) |n m. simple_if_graph (0, StartNode) (new_map simple_if_graph [IntVal 1, IntVal 20, IntVal 100]) \<mapsto> (n, m)}"
-values "{map m [0] |n m. eval_graph simple_if_graph (0, StartNode) (new_map simple_if_graph [IntVal 1, IntVal 20, IntVal 100]) (n, m)}"
+values "{(n, map m [0]) |n m. simple_if \<turnstile> (0, (new_map simple_if_graph [IntVal 1, IntVal 20, IntVal 100])) \<mapsto> (n, m)}"
+values "{map m [0] |n m. eval_graph simple_if (0, (new_map simple_if_graph [IntVal 1, IntVal 20, IntVal 100])) (n, m)}"
 
 end

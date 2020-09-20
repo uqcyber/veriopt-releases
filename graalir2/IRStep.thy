2cf6b478a030eefbf642eccec18bbd340fc861a2
Mark Utting <m.utting@uq.edu.au>
2020-09-21 08:24:57 +1000

    Ported the inductive step, step_top, exec and exec_debug over to the new graphs.

--- graalir2/IRStep.thy
+++ graalir2/IRStep.thy
@@ -1,4 +1,4 @@
-section \<open>Inductive small-step semantics of nodes\<close>
+section \<open>Inductive small-step semantics of IR graphs\<close>
 
 theory IRStep
   imports
@@ -6,7 +6,113 @@ theory IRStep
 begin
 
 
+text_raw \<open>\snip{step_semantics}{\<close>
+inductive
+  step :: "IRGraph \<Rightarrow> (ID \<times> MapState) \<Rightarrow> (ID \<times> MapState) \<Rightarrow> bool"
+  ("_ \<turnstile> _\<rightarrow>_" 55)
+  for g where
+
+  SequentialNode:
+  "\<lbrakk>node = kind g nid;
+    is_sequential_node(node);
+    next = (succ g nid)!0\<rbrakk> 
+    \<Longrightarrow> g \<turnstile> (nid, m) \<rightarrow> (next, m)" |
+
+  IfNode:
+  "\<lbrakk>kind g nid = (IfNode cond true false);
+    g m \<turnstile> cond (kind g cond) \<mapsto> (IntVal val);
+    next = (if val_to_bool val then true else false)\<rbrakk>
+    \<Longrightarrow> g \<turnstile> (nid, m) \<rightarrow> (next, m)" |  
+
+  EndNodes:
+  "\<lbrakk>is_end_node(kind g nid);
+
+    merge = any_usage g nid;
+    is_merge_node(kind g merge);
+
+    i = input_index g merge nid;
+
+    phis = (phi_list g merge);
+    inputs = (phi_inputs g i phis);
+    g inputs m \<longmapsto> vs;
+
+    m' = (set_phis phis vs m)\<rbrakk> 
+    \<Longrightarrow> g \<turnstile> (nid, m) \<rightarrow> (merge, m')"
+text_raw \<open>}%endsnip\<close>
+
+code_pred [show_modes] step .
 
 
+text_raw \<open>\snip{top_step_semantics}{\<close>
+inductive
+  step_top :: "IRGraph \<Rightarrow> (ID \<times> MapState) list \<Rightarrow> (ID \<times> MapState) list \<Rightarrow> bool"
+  ("_\<turnstile>_\<longrightarrow>_" 55) 
+  for g where
+
+  "\<lbrakk>g \<turnstile> (nid, m) \<rightarrow> (nid', m')\<rbrakk> 
+    \<Longrightarrow> g \<turnstile> (nid, m) # xs \<longrightarrow> (nid', m') # xs" |
+
+  CallNodeStep:
+  "\<lbrakk>kind g nid = (CallNode start _);
+    g (inp g nid) m \<longmapsto> vs;
+    m' =  set_params new_map_state vs\<rbrakk>
+    \<Longrightarrow> g \<turnstile> (nid, m)#xs \<longrightarrow> (start, m')#(nid,m)#xs" |
+
+  ReturnNode:
+  "\<lbrakk>kind g nid = (ReturnNode (Some expr) _);
+    g m \<turnstile> expr (kind g expr) \<mapsto> v;
+    c_m' = m_set c_nid v c_m;
+    c_nid' = (succ g c_nid)!0\<rbrakk> 
+    \<Longrightarrow> g \<turnstile> (nid,m)#(c_nid,c_m)#xs \<longrightarrow> (c_nid',c_m')#xs" |
+
+  ExitReturnNode:
+  "\<lbrakk>kind g nid = (ReturnNode (Some expr) _);
+    g m \<turnstile> expr (kind g expr) \<mapsto> v;
+    m' = m_set nid v m\<rbrakk> 
+    \<Longrightarrow> g \<turnstile> (nid, m)#[] \<longrightarrow> []"
+
+text_raw \<open>}%endsnip\<close>
+
+code_pred [show_modes] step_top .
+
+
+inductive exec :: "IRGraph \<Rightarrow> (ID \<times> MapState) list \<Rightarrow> (ID \<times> MapState) list \<Rightarrow> bool"
+  ("_ \<turnstile> _ \<longrightarrow>* _")
+  where
+  "\<lbrakk>g \<turnstile> s \<longrightarrow> s';
+    exec g s' s''\<rbrakk> 
+    \<Longrightarrow> exec g s s''" 
+(* TODO: this seems to be unnecessary? *)
+  |
+  "\<lbrakk>g \<turnstile> s \<longrightarrow> s';
+    length s' = 0\<rbrakk>
+    \<Longrightarrow> exec g s s"
+code_pred [show_modes] "exec" .
+
+
+inductive exec_debug :: "IRGraph \<Rightarrow> (ID \<times> MapState) list \<Rightarrow> nat \<Rightarrow> (ID \<times> MapState) list \<Rightarrow> bool"
+  ("_ \<turnstile> _ \<rightarrow>*_* _")
+  where
+  "\<lbrakk>n > 0;
+    g \<turnstile> s \<longrightarrow> s';
+    exec_debug g s' (n - 1) s''\<rbrakk> 
+    \<Longrightarrow> exec_debug g s n s''" |
+
+  "\<lbrakk>n = 0\<rbrakk>
+    \<Longrightarrow> exec_debug g s n s"
+code_pred [show_modes] "exec_debug" .
+
+
+
+definition call_eg2 :: IRGraph where
+  "call_eg2 = fmupd 10 (CallNode 0 []) eg2_sq"
+
+definition p3:: MapState where
+  "p3 = new_map [IntVal 3]"
+
+value "inp call_eg2 10"
+
+(* Eg. call eg2_sq with [3] \<longrightarrow> 9 *)
+values "{m_val (snd (hd res)) 0 | res. eg2_sq \<turnstile> [(0, p3), (0, p3)] \<rightarrow>*2* res}"
 end
 

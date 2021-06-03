38030f2843ac3f666c4cdf860e4c58217d9af033
Mark Utting <m.utting@uq.edu.au>
2021-06-03 15:36:55 +1000

    Version 2 of expression tree extraction and insertion into IRGraphs.  This still has issues with threading the new graph state through the inductive definition.

--- Semantics/IRTreeEval.thy
+++ Semantics/IRTreeEval.thy
@@ -61,9 +61,13 @@ subsection \<open>Data-flow Tree Representation\<close>
 fun fresh_id :: "IRGraph \<Rightarrow> ID \<Rightarrow> bool" where
   "fresh_id g nid = (nid \<notin> ids g)"
 
+
+datatype IRUnaryOp = UnaryAbs | UnaryNeg
+datatype IRBinaryOp = BinAdd | BinMul | BinSub
+
 datatype (discs_sels) IRExpr =
-  AbsExpr (ir_value: IRExpr) 
-  | AddExpr (ir_x: IRExpr) (ir_y: IRExpr) 
+  UnaryExpr (ir_uop: IRUnaryOp) (ir_value: IRExpr) 
+  | BinaryExpr (ir_op: IRBinaryOp) (ir_x: IRExpr) (ir_y: IRExpr) 
 (* TODO
   | AndExpr (ir_x: IRExpr) (ir_y: IRExpr)
   | ConditionalExpr (ir_condition: IRExpr) (ir_trueValue: IRExpr) (ir_falseValue: IRExpr) 
@@ -75,7 +79,6 @@ datatype (discs_sels) IRExpr =
   | IsNullNode (ir_value: IRExpr) 
   | LogicNegationNode (ir_value: IRExpr)
 *)
-  | MulExpr (ir_x: IRExpr) (ir_y: IRExpr) 
 (* TODO
   | NegateNode (ir_value: IRExpr) 
   | NotNode (ir_value: IRExpr) 
@@ -86,7 +89,6 @@ datatype (discs_sels) IRExpr =
   | PiNode (ir_object: IRExpr) (ir_guard_opt: "IRExpr option")
   | ShortCircuitOrNode (ir_x: IRExpr) (ir_y: IRExpr)
 *)
-  | SubExpr (ir_x: IRExpr) (ir_y: IRExpr) 
 (* Not needed?
   | UnwindNode (ir_exception: IRExpr) 
   | ValueProxyNode (ir_value: IRExpr) (ir_loopExit: IRExpr) 
@@ -114,25 +116,25 @@ inductive
   AbsNode:
   "\<lbrakk>kind g n = AbsNode x;
     g x \<triangleright> xe\<rbrakk>
-    \<Longrightarrow> g n \<triangleright> (AbsExpr xe)" |
+    \<Longrightarrow> g n \<triangleright> (UnaryExpr UnaryAbs xe)" |
 
   AddNode:
   "\<lbrakk>kind g n = AddNode x y;
     g x \<triangleright> xe;
     g x \<triangleright> ye\<rbrakk>
-    \<Longrightarrow> g n \<triangleright> (AddExpr xe ye)" |
+    \<Longrightarrow> g n \<triangleright> (BinaryExpr BinAdd xe ye)" |
 
   MulNode:
   "\<lbrakk>kind g n = MulNode x y;
     g x \<triangleright> xe;
     g x \<triangleright> ye\<rbrakk>
-    \<Longrightarrow> g n \<triangleright> (MulExpr xe ye)" |
+    \<Longrightarrow> g n \<triangleright> (BinaryExpr BinMul xe ye)" |
 
   SubNode:
   "\<lbrakk>kind g n = SubNode x y;
     g x \<triangleright> xe;
     g x \<triangleright> ye\<rbrakk>
-    \<Longrightarrow> g n \<triangleright> (SubExpr xe ye)" |
+    \<Longrightarrow> g n \<triangleright> (BinaryExpr BinSub xe ye)" |
 
   LoadFieldNode: (* TODO others *)
   "\<lbrakk>kind g n = LoadFieldNode nid f obj nxt;
@@ -145,87 +147,94 @@ values "{t. eg2_sq 4  \<triangleright> t}"
 
 
 (* ======== TODO: Functions for re-calculating stamps ========== *)
-fun stamp_abs :: "Stamp \<Rightarrow> Stamp" where "stamp_abs s = s"
-fun stamp_add :: "Stamp \<Rightarrow> Stamp \<Rightarrow> Stamp" where "stamp_add x y = x"
-fun stamp_sub :: "Stamp \<Rightarrow> Stamp \<Rightarrow> Stamp" where "stamp_sub x y = x"
-fun stamp_mul :: "Stamp \<Rightarrow> Stamp \<Rightarrow> Stamp" where "stamp_mul x y = x"
+fun stamp_unary :: "IRUnaryOp \<Rightarrow> Stamp \<Rightarrow> Stamp" where
+  "stamp_unary op s1 = s1"
+
+fun stamp_binary :: "IRBinaryOp \<Rightarrow> Stamp \<Rightarrow> Stamp \<Rightarrow> Stamp" where
+  "stamp_binary op s1 s2 = s1"
+
+(* TODO! implement these to find nodes, so we reuse nodes as much as possible. *)
+fun find_node :: "IRGraph \<Rightarrow> IRNode \<Rightarrow> ID option" where
+  "find_node g n = None"
+
+fun find_node_and_stamp :: "IRGraph \<Rightarrow> (IRNode \<times> Stamp) \<Rightarrow> ID option" where
+  "find_node_and_stamp g (n,s) = None"
+
+(* Creates the appropriate IRNode for a given binary operator. *)
+fun bin_node :: "IRBinaryOp \<Rightarrow> ID \<Rightarrow> ID \<Rightarrow> IRNode" where
+  "bin_node BinAdd x y = AddNode x y" |
+  "bin_node BinMul x y = MulNode x y" |
+  "bin_node BinSub x y = SubNode x y"
 
 
-(* First version of tree insertion into graph:
+(* Second version of tree insertion into graph:
 
-      g n \<triangleleft> expr \<leadsto> g' re-inserts expr into g, rooted at node ID n.
+      g \<triangleleft> expr \<leadsto> (g',nid) re-inserts expr into g and returns the new root nid.
 
-   This means that we recurse into the expression tree allocating node IDs top-down,
-   so every leaf gets given a new ID.
-
-   An alternative (better?) approach would be to decide the node IDs bottom-up,
-   so that leaf IDs can be re-used, and intermediate nodes can also be re-used
-   if they have not changed. 
- *)
+   This means that we can try to re-use existing nodes by finding node IDs bottom-up.
+*)
 inductive
-  unrep :: "IRGraph \<Rightarrow> ID \<Rightarrow> IRExpr \<Rightarrow> IRGraph \<Rightarrow> bool" ("_ _ \<triangleleft> _ \<leadsto> _" 55)
+  unrep :: "IRGraph \<Rightarrow> IRExpr \<Rightarrow> (IRGraph \<times> ID) \<Rightarrow> bool" ("_ \<triangleleft> _ \<leadsto> _" 55)
   for g where
 
-  ConstantNode:
-  "\<lbrakk>constant_as_stamp c = cstamp;
-    g' = add_node n (ConstantNode c, cstamp) g \<rbrakk>
-    \<Longrightarrow> g n \<triangleleft> (ConstantExpr c) \<leadsto> g'" |
+  ConstantNodeSame:
+  "\<lbrakk>find_node_and_stamp g (ConstantNode c, constant_as_stamp c) = Some nid\<rbrakk>
+    \<Longrightarrow> g \<triangleleft> (ConstantExpr c) \<leadsto> (g, nid)" |
 
-  ParameterNode:
-  "\<lbrakk>g' = add_node n (ParameterNode i, s) g\<rbrakk>
-    \<Longrightarrow> g n \<triangleleft> (ParameterExpr i s) \<leadsto> g'" |
+  ConstantNodeNew:
+  "\<lbrakk>find_node_and_stamp g (ConstantNode c, constant_as_stamp c) = None;
+    fresh_id g nid;
+    g' = add_node nid (ConstantNode c, constant_as_stamp c) g \<rbrakk>
+    \<Longrightarrow> g \<triangleleft> (ConstantExpr c) \<leadsto> (g', nid)" |
 
-  AbsNode:
-  "\<lbrakk>fresh_id g nid2;
-    g nid2 \<triangleleft> xe \<leadsto> g2;
-    s2 = stamp g2 nid2;
-    s' = stamp_abs s2; 
-    g' = add_node n (AbsNode nid2, s') g2\<rbrakk>
-    \<Longrightarrow> g n \<triangleleft> (AbsExpr xe) \<leadsto> g'" |
 
-  AddNode:
-  "\<lbrakk>fresh_id g nidx;
-    g nidx \<triangleleft> xe \<leadsto> gx;
-    sx = stamp gx nidx;
-    fresh_id gx nidy;
-    g nidy \<triangleleft> ye \<leadsto> gy;
-    sy = stamp gy nidy;
-    s' = stamp_add sx sy; 
-    g' = add_node n (AddNode nidx nidy, s') gy\<rbrakk>
-    \<Longrightarrow> g n \<triangleleft> (AddExpr xe ye) \<leadsto> g'" |
+  ParameterNodeSame:
+  "\<lbrakk>find_node_and_stamp g (ParameterNode i, s) = Some nid\<rbrakk>
+    \<Longrightarrow> g \<triangleleft> (ParameterExpr i s) \<leadsto> (g, nid)" |
 
-  MulNode:
-  "\<lbrakk>fresh_id g nidx;
-    g nidx \<triangleleft> xe \<leadsto> gx;
-    sx = stamp gx nidx;
-    fresh_id gx nidy;
-    g nidy \<triangleleft> ye \<leadsto> gy;
-    sy = stamp gy nidy;
-    s' = stamp_sub sx sy; 
-    g' = add_node n (MulNode nidx nidy, s') gy\<rbrakk>
-    \<Longrightarrow> g n \<triangleleft> (MulExpr xe ye) \<leadsto> g'" |
+  ParameterNodeNew:
+  "\<lbrakk>find_node_and_stamp g (ParameterNode i, s) = None;
+    fresh_id g nid;
+    g' = add_node nid (ParameterNode i, s) g\<rbrakk>
+    \<Longrightarrow> g \<triangleleft> (ParameterExpr i s) \<leadsto> (g', nid)" |
 
-  SubNode:
-  "\<lbrakk>fresh_id g nidx;
-    g nidx \<triangleleft> xe \<leadsto> gx;
-    sx = stamp gx nidx;
-    fresh_id gx nidy;
-    g nidy \<triangleleft> ye \<leadsto> gy;
-    sy = stamp gy nidy;
-    s' = stamp_mul sx sy; 
-    g' = add_node n (SubNode nidx nidy, s') gy\<rbrakk>
-    \<Longrightarrow> g n \<triangleleft> (SubExpr xe ye) \<leadsto> g'" |
 
-  AllLeafNodes: (* TODO check that kind g nid is indeed a leaf node?
-    e.g.  kind g nid = LoadFieldNode nid f obj nxt;
- *)
-  "\<lbrakk>kind g nid = node;
-    stamp g nid = s;
-    fresh_id g nid2;
-    g' = add_node nid2 (node, s) g\<rbrakk>
-    \<Longrightarrow> g n \<triangleleft> (LeafExpr nid s) \<leadsto> g'"
+  AbsNodeSame:
+  "\<lbrakk>g \<triangleleft> xe \<leadsto> (g2, x);
+    find_node_and_stamp g2 (AbsNode x, stamp g2 x) = Some nid\<rbrakk>
+    \<Longrightarrow> g \<triangleleft> (UnaryExpr UnaryAbs xe) \<leadsto> (g2, nid)" |
 
-code_pred (modes: i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> bool as unrepE) unrep .
+  AbsNodeNew:
+  "\<lbrakk>g \<triangleleft> xe \<leadsto> (g2, x);
+    find_node_and_stamp g2 (AbsNode x, stamp g2 x) = None;
+    fresh_id g2 nid;
+    s' = stamp_unary UnaryAbs (stamp g2 x);
+    g' = add_node nid (AbsNode nidx, s') g2\<rbrakk>
+    \<Longrightarrow> g \<triangleleft> (UnaryExpr UnaryAbs xe) \<leadsto> (g', nid)" |
+
+
+  BinaryNodeSame:
+  "\<lbrakk>g \<triangleleft> xe \<leadsto> (g2, x);
+    g2 \<triangleleft> ye \<leadsto> (g3, y);
+    s' = stamp_binary op (stamp g3 x) (stamp g3 y);
+    find_node_and_stamp g3 (bin_node op x y, s') = Some nid\<rbrakk>
+    \<Longrightarrow> g \<triangleleft> (BinaryExpr op xe ye) \<leadsto> (g3, nid)" |
+
+  BinaryNodeNew:
+  "\<lbrakk>g \<triangleleft> xe \<leadsto> (g2, x);
+    g2 \<triangleleft> ye \<leadsto> (g3, y);
+    s' = stamp_binary op (stamp g3 x) (stamp g3 y);
+    find_node_and_stamp g3 (bin_node op x y, s') = None;
+    fresh_id g3 nid;
+    g' = add_node nid (bin_node op x y, s') g3\<rbrakk>
+    \<Longrightarrow> g \<triangleleft> (BinaryExpr op xe ye) \<leadsto> (g', nid)" |
+
+
+  AllLeafNodes:
+  "stamp g nid = s
+    \<Longrightarrow> g \<triangleleft> (LeafExpr nid s) \<leadsto> (g, nid)"
+
+code_pred (modes: i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> bool as unrepE) unrep .
 
 values "{t. eg2_sq 4  \<triangleright> t}"
 

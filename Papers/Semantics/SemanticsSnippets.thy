30c5d8171b228bdb8e0ea81f94f7f627507d4296
Brae Webb <email@braewebb.com>
2021-06-15 15:00:57 +1000

    Reorganise sessions
    Nest all documentation related sessions into
    the Papers directory.
    Changed snippet files from a conference naming
    system to a topic naming system.

--- Papers/Semantics/SemanticsSnippets.thy
+++ Papers/Semantics/SemanticsSnippets.thy
@@ -1,4 +1,4 @@
-theory ATVA2021
+theory SemanticsSnippets
   imports
     Optimizations.CanonicalizationProofs
 begin
@@ -232,7 +232,7 @@ lemma CanonicalizeIfProof_fake:
   assumes "g' = replace_node_fake nid after g"
   assumes "g \<turnstile> (nid, m, h) \<rightarrow> (nid', m, h)"
   shows "nid | g \<sim> g'"
-  sorry
+  by (metis CanonicalizeIfProof assms(1) assms(2) assms(3) assms(4) replace_node_fake_def)
 
 text_raw \<open>\Snip{CanonicalizeIfNodeProof}%
 \begin{center}
